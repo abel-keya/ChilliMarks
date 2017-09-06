26052d7fb940b20dc8c59f36c5982df144726455
Valentine <valentinedeveloper@gmail.com>
2017-09-06 11:42:02 +0300

    Add Exam Name placeholder

--- resources/views/core/exams/create.blade.php
+++ resources/views/core/exams/create.blade.php
@@ -27,7 +27,7 @@
 	    <h5 class="pull-right">Exam Name</h5>
 	</div>
 	<div class="padded-full">
-		<input type="text" name="name" value="{{ old('name') }}" autocomplete="off" placeholder="Enter Exam Name" autofocus>
+		<input type="text" name="name" value="{{ old('name') }}" autocomplete="off" placeholder="Exam Name i.e. End Term" autofocus>
 	</div>
 	<div class="padded-full">
 		<select name="subject_id">
